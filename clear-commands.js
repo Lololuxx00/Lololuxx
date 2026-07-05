c1762cffd17e9990f4f2d7eba2ebe04697158d2a
Lololuxx00 <tomcode58@gmail.com>
2026-07-05 19:02:25 +0200

    Ajout bouton Disney

--- clear-commands.js
+++ clear-commands.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 require("dotenv").config();
+
 const { REST, Routes } = require("discord.js");
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
@@ -17,24 +17,25 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
     } catch (err) {
         console.error(err);
     }
-=======
-require("dotenv").config();
-const { REST, Routes } = require("discord.js");
-
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
-
-(async () => {
-    try {
-        console.log("Suppression des commandes globales...");
-
-        await rest.put(
-            Routes.applicationCommands(process.env.CLIENT_ID),
-            { body: [] }
-        );
-
-        console.log("✅ Commandes globales supprimées !");
-    } catch (err) {
-        console.error(err);
-    }
->>>>>>> f0e516e748b4c60f8c9c7a4aea40baefe997c30a
 })();
+
+require("dotenv").config();
+const { REST, Routes } = require("discord.js");
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log("Suppression des commandes globales...");
+
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: [] }
+        );
+
+        console.log("✅ Commandes globales supprimées !");
+    } catch (err) {
+        console.error(err);
+    }
+
+})()
